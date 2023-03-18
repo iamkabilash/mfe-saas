76b34ae3b37b5363fb0a1fe1999f2e4195242e5f
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-18 19:58:52 +0530

    completed generic ties between marketing and container

---
+++ packages/container/config/webpack.dev.js
@@ -0,0 +1,31 @@
+const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+
+const packageJson = require("../package.json");
+
+const devConfig = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+    historyApiFallback: {
+      index: "index.html",
+    },
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+      // shared: ["react", "react-dom"], // instead of writing one by one, we write below line.
+      shared: packageJson.dependencies,
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, devConfig); // 2nd param takes priority, so now devConfig can overwrite commonConfig while merging.

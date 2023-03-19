2e12d3645bf63161c1ba378eed07df8664e937f2
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-19 22:11:42 +0530

    completed navigation between container and marketing

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,25 @@
 import { mount } from "marketing/MarketingApp";
 import React, { useRef, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 function MarketingApp() {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        // getting apthname as nextpathname
+        // console.log(nextPathname);
+
+        const { pathname } = history.location;
+        // to prevent infinite page.
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;

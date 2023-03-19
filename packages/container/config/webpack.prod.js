e2ebe16a4c8f74831819b0eb04d5bd69fa751b8f
Kabilash Satheesh Kumar <iamkabilash@gmail.com>
2023-03-19 18:56:47 +0530

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,12 +9,13 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/", // deployment looks for main.js file in this path. so we add this line.
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
-        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
       },
       shared: packageJson.dependencies,
     }),
